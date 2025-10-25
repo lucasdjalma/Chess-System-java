dc0f4ec0b8b57d68e14db9940e4395baec60f47b
lucasdjalma <rhino2057@gmail.com>
2025-10-25 01:08:43 -0300

    implementing possible moves of rook

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -4,71 +4,88 @@ import boardgame.Board;
 import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
-// classe que representa uma partida de xadrez
+
+// Classe que representa uma partida de xadrez
 public class ChessMatch {
-	// atributo que representa o tabuleiro de xadrez
-	private Board board;
-	// construtor que inicializa o tabuleiro de xadrez e configura a posição inicial das peças
-	public ChessMatch() {
-		board = new Board(8, 8);
-		initialSetup();
-	}
-	// retorna uma matriz bidimensional com as peças do tabuleiro de xadrez
-	public ChessPiece[][] getPieces() {
-		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-		for (int i = 0; i < board.getRows(); i++) {
-			for (int j = 0; j < board.getColumns(); j++) {
-				mat[i][j] = (ChessPiece) board.piece(i, j);
-			}
-		}
-		return mat;
-	}
-	// executa um movimento de xadrez da posição de origem para a posição de destino
-	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
-		Position source = sourcePosition.toPosition();
-		Position target = targetPosition.toPosition();
-		validateSourcePosition(source);
-		ChessPiece capturedPiece = makeMove(source, target);
-		return capturedPiece; 
-	}
-	// valida se há uma peça na posição de origem
-	private void validateSourcePosition(Position position) {
-		if (!board.thereIsAPiece(position)) {
-			throw new ChessException("There is no piece on source position");
-		}
-		if (!board.piece(position).isThereAnyPossibleMove()) {
-			throw new ChessException("There is no possible moves for the chosen piece");
-		}
-	}
-	// realiza o movimento da peça no tabuleiro de xadrez
-	private ChessPiece makeMove(Position source, Position target) {
-		ChessPiece p = (ChessPiece) board.removePiece(source);
-		ChessPiece capturedPiece = (ChessPiece) board.removePiece(target);
-		board.placePiece(p, target);
-		return capturedPiece;
-	}
-	// coloca uma nova peça no tabuleiro de xadrez na coluna e linha especificadas 
-	private void placeNewPiece(char column, int row, ChessPiece piece) {
-		board.placePiece(piece, new ChessPosition(column, row).toPosition());
-	}
-	// configuração inicial do tabuleiro de xadrez
-	private void initialSetup(){
-		// White pieces
-		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
-		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
-		placeNewPiece('d', 2, new Rook(board, Color.WHITE));
-		placeNewPiece('e', 2, new Rook(board, Color.WHITE));
-		placeNewPiece('e', 1, new Rook(board, Color.WHITE));
-		placeNewPiece('d', 1, new King(board, Color.WHITE));
-		
-		// Black pieces
-		placeNewPiece('c', 7, new Rook(board, Color.BLACK));
-		placeNewPiece('c', 8, new Rook(board, Color.BLACK));
-		placeNewPiece('d', 7, new Rook(board, Color.BLACK));
-		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
-		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
-		placeNewPiece('d', 8, new King(board, Color.BLACK));
-				
+
+    // Atributo que representa o tabuleiro de xadrez
+    private Board board;
+
+    // Construtor que inicializa o tabuleiro e define as peças iniciais
+    public ChessMatch() {
+        board = new Board(8, 8);
+        initialSetup();
+    }
+
+    // Retorna uma matriz bidimensional com as peças do tabuleiro
+    public ChessPiece[][] getPieces() {
+        ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+        for (int i = 0; i < board.getRows(); i++) {
+            for (int j = 0; j < board.getColumns(); j++) {
+                mat[i][j] = (ChessPiece) board.piece(i, j);
+            }
+        }
+        return mat;
+    }
+
+    // Executa um movimento de xadrez da origem para o destino
+    public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+        Position source = sourcePosition.toPosition();
+        Position target = targetPosition.toPosition();
+
+        validateSourcePosition(source);
+        validateTargetPosition(source, target);
+
+        ChessPiece capturedPiece = makeMove(source, target);
+        return capturedPiece;
+    }
+
+    // Valida se há uma peça na posição de origem
+    private void validateSourcePosition(Position position) {
+        if (!board.thereIsAPiece(position)) {
+            throw new ChessException("There is no piece on source position");
+        }
+        if (!board.piece(position).isThereAnyPossibleMove()) {
+            throw new ChessException("There is no possible moves for the chosen piece");
+        }
+    }
+
+    // Valida se o movimento é possível
+    private void validateTargetPosition(Position source, Position target) {
+        if (!board.piece(source).possibleMove(target)) {
+            throw new ChessException("The chosen piece can't move to target position");
+        }
+    }
+
+    // Realiza o movimento no tabuleiro
+    private ChessPiece makeMove(Position source, Position target) {
+        ChessPiece p = (ChessPiece) board.removePiece(source);
+        ChessPiece capturedPiece = (ChessPiece) board.removePiece(target);
+        board.placePiece(p, target);
+        return capturedPiece;
+    }
+
+    // Coloca uma nova peça no tabuleiro
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
+    // Configuração inicial do tabuleiro de xadrez
+    private void initialSetup() {
+        // Peças brancas
+        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        // Peças pretas
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
+    }
 }
-	
-	}

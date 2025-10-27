910de8a1fc3846f5c2655a6a864e8cfc0ae52111
Lucas Djalma <lucas.djalmasl@gmail.com>
2025-10-27 00:29:27 -0300

    Handling captured pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessMatch;
@@ -45,8 +46,10 @@ public class UI {
         }
     }
     
-    public static void printMatch(ChessMatch chessMatch) {
+    public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
 		printBoard(chessMatch.getPieces());
+		System.out.println();
+		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
@@ -88,5 +91,20 @@ public class UI {
             }
         }
         System.out.print(ANSI_RESET);
+    }
+    
+    private static void printCapturedPieces(List<ChessPiece> captured) {
+    		    List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == chess.Color.WHITE).toList();
+    			List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == chess.Color.BLACK).toList();
+    			System.out.println("Captured pieces:");
+    			System.out.print("White: ");
+    			System.out.print(ANSI_WHITE);
+    			System.out.print(java.util.Arrays.toString(white.toArray()));
+    			System.out.println(ANSI_RESET);
+    			System.out.print("Black: ");
+    			System.out.print(ANSI_YELLOW);
+    			System.out.print(java.util.Arrays.toString(black.toArray()));
+    			System.out.println(ANSI_RESET);
+    			
     }
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
 import boardgame.Position;
 import chess.pieces.King;
@@ -12,6 +15,9 @@ public class ChessMatch {
 	private int turn;
 	private Color currentPlayer;
     private Board board;
+    
+    private List<ChessPiece> piecesOnTheBoard = new ArrayList<>();
+    private List<ChessPiece> capturedPieces = new ArrayList<>();
 
     // Construtor que inicializa o tabuleiro e define as peças iniciais
     public ChessMatch() {
@@ -90,12 +96,17 @@ public class ChessMatch {
         ChessPiece p = (ChessPiece) board.removePiece(source);
         ChessPiece capturedPiece = (ChessPiece) board.removePiece(target);
         board.placePiece(p, target);
+        if (capturedPiece != null) {
+			piecesOnTheBoard.remove(capturedPiece);
+			capturedPieces.add(capturedPiece);
+		}
         return capturedPiece;
     }
 
     // Coloca uma nova peça no tabuleiro
     private void placeNewPiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+        piecesOnTheBoard.add(piece);
     }
 
     // Configuração inicial do tabuleiro de xadrez
